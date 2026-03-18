17ba7ac0f80edd0514a5b2fdb4a1562b8b66f2d2
HungPhat302 <hungphatbui99@gmail.com>
2026-03-18 20:00:50 +0700

    [update] Feature search posts by keywords, category, tag

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,13 +1,15 @@
 // Chưa đủ API
 
 /* 
-1. Lấy danh sách bài viết publish
-2. Chi tiết bài post
-3. Lấy danh sách bài viết theo category
-4. Lấy danh sách bài viết theo tag
+1. Lấy danh sách bài viết publish x
+2. Chi tiết bài post x
+3. Lấy danh sách bài viết theo category 
+4. Lấy danh sách bài viết theo tag 
 */
 
 const Post = require("../models/Post.model");
+const Category = require("../models/Category.model");
+const Tag = require("../models/Tag.model");
 
 // Lấy tất cả bài posts
 exports.getPosts = async (_req, res) => {
@@ -23,7 +25,108 @@ exports.getPosts = async (_req, res) => {
         })
     }
 }
-//tạo 
+
+// Đọc chi tiết bài post
+exports.getDetailPost = async (req, res) => {
+    try {
+        const detailPost = await Post.findById(req.params.id).select("title content slug category tags image author publishdate");
+        return res.status(200).json({
+            message: "Successfully to get content",
+            data: detailPost
+        })
+    } catch(error) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+// Lấy các posts theo category
+exports.getPostByCategory = async (req, res) => {
+    try {
+        const { categorySlug } = req.params;
+        if(!categorySlug) {
+            return res.status(400).json({
+                message: "Bad request"
+            });
+        }
+        const category_id = await Category.findOne({ slug: categorySlug }).select("_id");
+
+        if(!category_id) {
+            return res.status(400).json({
+                message: "Category not found"
+            })
+        }
+        const posts =  await Post.find({ category: category_id._id });
+
+        return res.status(200).json({
+            message: "Successfully get posts by category",
+            data: posts
+        })
+    } catch(error) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+// Lấy các posts theo tag
+exports.getPostByTag = async (req, res) => {
+    try {
+        const { tagSlug } = req.params;
+        if(!tagSlug) {
+            return res.status(400).json({
+                message: "Bad request"
+            });
+        }
+        const tag_id = await Tag.findOne({ slug: tagSlug }).select("_id");
+
+        if(!tag_id) {
+            return res.status(400).json({
+                message: "Tag not found"
+            })
+        }
+        const posts =  await Post.find({ tags: tag_id._id });
+
+        return res.status(200).json({
+            message: "Successfully get posts by tag",
+            data: posts
+        })
+    } catch(error) {
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+// Search post theo keyword
+exports.getPostByKeyword = async (req, res) => {
+    try {
+        const { keywords } = req.params;
+        if(!keywords) {
+            return res.status(400).json({
+                message: "Bad request"
+            });
+        }
+        const keywordArray = keywords.split(" ");
+        const posts =  await Post.find({
+            $or: keywordArray.map(word => ({
+                title: { $regex: word, $options: "i"}
+            }))
+        }).limit(10);
+
+        return res.status(200).json({
+            message: "Successfully",
+            data: posts
+        })
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+};
+
 // Tạo post mới
 exports.createPost = async (req, res) => {
     try {
@@ -144,30 +247,4 @@ exports.deletePost = async (req, res) => {
       message: "Internal Server Error"
     });
   }
-};
-
-// Search post theo keyword
-exports.getPostByKeyword = async (req, res) => {
-    try {
-        const { keyword } = req.params;
-        const keywordRegex = new RegExp(keyword);
-        const posts =  await Post.find({ title: keywordRegex});
-
-        // Validate
-        if(!keyword) {
-            return res.status(400).json({
-                message: "Bad request"
-            });
-        }
-
-        return res.status(200).json({
-            message: "Successfully",
-            data: posts
-        })
-    } catch(error) {
-        console.log(error);
-        return res.status(500).json({
-            message: "Internal server error"
-        });
-    }
 };
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -5,11 +5,16 @@ const upload = require("../middlewares/upload.middleware");
 const router = express.Router();
 // CRUD đơn giản cho post
 router.get("/", controller.getPosts);
+router.get("/:id", controller.getDetailPost);
 router.post("/", upload.single("image"), controller.createPost);
 router.put("/:id", upload.single("image"), controller.updatePost);
 router.delete("/:id", controller.deletePost);
 
 // search post theo keyword
-router.get("/:keyword", controller.getPostByKeyword);
+router.get("/search/:keywords", controller.getPostByKeyword);
+// search post theo category
+router.get("/category/:categorySlug", controller.getPostByCategory);
+// search post theo tag
+router.get("/tag/:tagSlug", controller.getPostByTag);
 
 module.exports = router;
