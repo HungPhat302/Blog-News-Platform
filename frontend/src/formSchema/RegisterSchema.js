dafa461770467bf688ff633b107577059c5bfe9b
newmindarchitech <phamtruonggiang726@gmail.com>
2026-03-26 21:19:08 +0700

    Update newfrontend branch with validations for register and login

---
+++ frontend/src/formSchema/RegisterSchema.js
@@ -0,0 +1,58 @@
+import * as Joi from "joi"
+
+
+
+const RegisterSchema = Joi.object().keys({
+    username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net',] } }) // This is validation regarding Top Level Domain of a domain name (com,net,...)
+        .required()
+        .custom((value, helpers) => {
+            const allowedDomains = ["gmail", "yahoo", "company"]; //This is validation regarding the name of the domain name (gmail,yahoo,...)
+
+            const [, domainPart] = value.split("@");
+            const domainName = domainPart.split(".")[0];
+
+            if (!allowedDomains.includes(domainName)) {
+                return helpers.error("email.domain");
+            }
+
+            return value;
+        })
+        .messages({
+            "string.email": "Email must be valid and use an allowed TLD (only .com or .net)",
+            "email.domain": "This email domain is not allowed (only gmail, yahoo, company)"
+        }),
+
+    password: Joi.string()
+        .alphanum()
+        .min(8)
+        .required(),
+
+    password_confirm: Joi.string()
+        .alphanum()
+        .min(8)
+        .required()
+        .custom((value, helpers) => {
+            const { password } = helpers.state.ancestors[0];
+
+            if (value !== password) {
+                return helpers.error("any.custom", {
+                    passwordValue: password,
+                });
+            }
+
+            return value;
+        })
+        .messages({
+            "any.custom": "Passwords do not match",
+        })
+})
+
+
+export default RegisterSchema
